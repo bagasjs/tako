c2e2c63179d083d7f019c3c4e4ca5719a214300a
bagasjs <nakanoyoichi83@gmail.com>
2024-05-15 23:43:29 +0700

    Simplify-ing error

--- core/errors.go
+++ core/errors.go
@@ -1,12 +1,22 @@
 package core
 
-type Error struct {
+type errorImpl struct {
     Message string `json:"message"`
     Code int `json:"code"`
 }
 
-func NewError(code int, message string) *Error {
-    return &Error {
+
+type Error *errorImpl
+
+func NewError(message string) Error {
+    return &errorImpl {
+        Message: message,
+        Code: 0,
+    }
+}
+
+func NewErrorWithCode(code int, message string) Error {
+    return &errorImpl {
         Code: code,
         Message: message,
     }
